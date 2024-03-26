418579585958e201ac68607402e8143072f6b245
Jens Böckmann <jensboe@outlook.de>
2024-03-26 01:02:45 +0100

    Disable router trace

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes,
     {
-      enableTracing: true, // <-- debugging purposes only
+      enableTracing: false, // <-- debugging purposes only
     }
   )
 ],
