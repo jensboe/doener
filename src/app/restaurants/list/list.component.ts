b527af966f94f479fe386222570f6fba28f5bb5c
Jens Böckmann <jensboe@outlook.de>
2024-03-26 01:13:38 +0100

    Add connection to firebase

--- src/app/restaurants/list/list.component.ts
+++ src/app/restaurants/list/list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Restaurant } from '../restaurant';
 import { RESTAURANTS } from '../mock.restaurants';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -8,5 +10,11 @@ import { RESTAURANTS } from '../mock.restaurants';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  restaurants: Restaurant[] = RESTAURANTS;
+  firestore: Firestore = inject(Firestore)
+  restaurants$: Observable<Restaurant[]>;
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'restaurants')
+    this.restaurants$ = collectionData(aCollection) as Observable<Restaurant[]>;
+  }
 }
