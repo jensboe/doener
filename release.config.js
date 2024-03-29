cc1a1968d373121d123a6ba842d6a277b39a7af0
Jens <jensboe@outlook.de>
2024-03-29 20:01:26 +0100

    fix(ci): Cleaning release process (#28)
    * fix(build): Remove build artefacts
    
    * fix(build): Remove build artefact creation form relase cfg
    
    * fix(build): Add package.json to sematic release assets

--- release.config.js
+++ release.config.js
@@ -15,7 +15,10 @@ module.exports = {
       [
         '@semantic-release/git',
         {
-          assets: ['CHANGELOG.md', 'dist/**'],
+          assets: [
+            'CHANGELOG.md',
+            'package.json'
+          ],
           message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
         }
       ]
