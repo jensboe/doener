d54d6bdf9756406a188c4874d0dc32a57b114e25
Jens Böckmann <jensboe@outlook.de>
2024-03-29 20:57:04 +0100

    feat(lang): Show currency € instead of $

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, importProvidersFrom } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, NgModule, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -17,36 +17,40 @@ import { DishesModule } from './dishes/dishes.module';
 
 
 @NgModule({
-    imports: [
-      BrowserModule,
-      BrowserAnimationsModule,
-      RestaurantsModule,
-      DishesModule,
-      AppRoutingModule,
-      MatToolbarModule,
-      MatButtonModule,
-      MatSidenavModule,
-      MatIconModule,
-      MatListModule
-    ],
-    declarations: [
-      AppComponent,
-    ],
-    bootstrap: [ AppComponent ],
-    providers:
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RestaurantsModule,
+    DishesModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  declarations: [
+    AppComponent,
+  ],
+  bootstrap: [AppComponent],
+  providers:
     [
-        importProvidersFrom(provideFirebaseApp(() => initializeApp({
-          "projectId":"doener-96c7a",
-          "appId":"1:194409694404:web:22fb20b08a51ab26a53591",
-          "storageBucket":"doener-96c7a.appspot.com",
-          "apiKey":"AIzaSyAdmhyijYOZsjiX6zzGcZb1CW6SvifyYKU",
-          "authDomain":"doener-96c7a.firebaseapp.com",
-          "messagingSenderId":"194409694404",
-          "measurementId":"G-7EV8PVRVZ8"
-        }))),
-        importProvidersFrom(provideFirestore(() => getFirestore())),
-        provideAnimationsAsync(),
+      importProvidersFrom(provideFirebaseApp(() => initializeApp({
+        "projectId": "doener-96c7a",
+        "appId": "1:194409694404:web:22fb20b08a51ab26a53591",
+        "storageBucket": "doener-96c7a.appspot.com",
+        "apiKey": "AIzaSyAdmhyijYOZsjiX6zzGcZb1CW6SvifyYKU",
+        "authDomain": "doener-96c7a.firebaseapp.com",
+        "messagingSenderId": "194409694404",
+        "measurementId": "G-7EV8PVRVZ8"
+      }))),
+      importProvidersFrom(provideFirestore(() => getFirestore())),
+      provideAnimationsAsync(),
+      {
+        provide: DEFAULT_CURRENCY_CODE,
+        useValue: 'EUR'
+      }
     ]
   })
-  export class AppModule {
-  }
+export class AppModule {
+}
