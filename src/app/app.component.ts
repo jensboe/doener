3bc16d05eadb8d66f1604059f1189c3bf1e36e28
Jens Böckmann <jensboe@outlook.de>
2024-03-29 22:14:54 +0100

    style: show current app version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,7 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class AppComponent {
   private breakpointObserver = inject(BreakpointObserver);
-
+  version = packageJson.version;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
