b16cc222bb37a163f1ff3e11790276cf9d479701
Jens Böckmann <jensboe@outlook.de>
2024-03-30 21:34:50 +0100

    style(menu): add login/logout/register link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,41 @@
 import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import packageJson from '../../package.json';
-import { AsyncPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatNavList, MatListItem } from '@angular/material/list';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatSidenavContainer, MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
+import { AuthService } from './auth.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     standalone: true,
-    imports: [MatSidenavContainer, MatSidenav, MatToolbar, MatNavList, MatListItem, RouterLink, RouterLinkActive, MatSidenavContent, MatIconButton, MatIcon, RouterOutlet, AsyncPipe]
+    imports: [
+      MatSidenavContainer,
+      MatSidenav,
+      MatToolbar,
+      MatNavList,
+      MatListItem,
+      RouterLink,
+      RouterLinkActive,
+      MatSidenavContent,
+      MatIconButton,
+      MatIcon,
+      RouterOutlet,
+      CommonModule
+    ]
 })
 export class AppComponent {
   private breakpointObserver = inject(BreakpointObserver);
+  public auth: AuthService = inject(AuthService);
+   
   version = packageJson.version;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
