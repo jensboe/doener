b1ee2433a52764df0f9f7cc7d44a167074ccd56d
Jens Böckmann <jensboe@outlook.de>
2024-03-31 17:58:03 +0200

    refactor(test): Fix tests for user

--- src/app/user/login/login.component.spec.ts
+++ src/app/user/login/login.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../auth.service';
+import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  const AuthServiceStub: Partial<AuthService> = {
+    loggedIn$: of(true)
+  }
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [LoginComponent],
+      providers: [{ provide: AuthService, useValue: AuthServiceStub }],
     })
     .compileComponents();
     
--- src/app/user/register/register.component.spec.ts
+++ src/app/user/register/register.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { AuthService } from '../../auth.service';
+import { of } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  const AuthServiceStub: Partial<AuthService> = {
+    loggedIn$: of(true)
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterComponent]
+      imports: [
+        RegisterComponent,
+        BrowserAnimationsModule
+      ],
+      providers: [{ provide: AuthService, useValue: AuthServiceStub }],
     })
     .compileComponents();
     
